f26c12c879ec1a2b2988e272c3f3502cbbe93280
Saurabh Varadkar <saurabh.varadkar@gmail.com>
2024-11-04 00:40:18 +0530

    port in .env

--- app.py
+++ app.py
@@ -338,8 +338,7 @@ def post(post_id):
         flash("Post not found.", "warning")
         return redirect(url_for("home"))
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__":    
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000
     app.run(host="0.0.0.0", port=port)
 
